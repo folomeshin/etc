1d1c91aa68bc0aa0a0a998b17763bdd01d9cae00
Alexandre Folomechine <alex.folomechine@blueprintsys.com>
2019-02-19 15:35:47 -0500

    Updated the query for Saturn reports, included only Rocket issues done in Saturn sprints.

--- blueprint-jira-better-excel/blueprint-saturn-planning-helper.groovy
+++ blueprint-jira-better-excel/blueprint-saturn-planning-helper.groovy
@@ -1,2 +1,2 @@
 filename = "Saturn Release Planning - ${new Date().format("yyyy-MM-dd-HH-mm-ss-z", TimeZone.getTimeZone('EST'))}.xlsx".toString();
-issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND fixVersion in (Rocket, Saturn) AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
+issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND (fixVersion in (Saturn) OR (fixVersion = Rocket AND (Sprint in (116, 117, 118) OR Sprint is EMPTY ))) AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
