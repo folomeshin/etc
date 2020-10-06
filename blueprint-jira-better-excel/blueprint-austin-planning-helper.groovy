8fcd9688eed450a03e074b43c863172c36f1e3bc
Alex Folomechine <alex.folomechine@blueprintsys.com>
2020-10-05 21:50:01 -0400

    Added for Austin.

---
+++ blueprint-jira-better-excel/blueprint-austin-planning-helper.groovy
@@ -0,0 +1,2 @@
+filename = "Austin Release Planning - ${new Date().format("yyyy-MM-dd-HH-mm-ss-z", TimeZone.getTimeZone('EST'))}.xlsx".toString();
+issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND (fixVersion in (Austin) OR (fixVersion = Austin AND (Sprint in (160, 161, 162, 163, 164) OR Sprint is EMPTY ))) AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
