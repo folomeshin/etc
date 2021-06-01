d8d5feba2ff2345e0588daee26ec388042d0ce9f
Alex Folomechine <alex.folomechine@blueprintsys.com>
2021-06-01 16:08:30 -0400

    The last versions for Belarus.

--- blueprint-jira-better-excel/blueprint-belarus-planning-helper.groovy
+++ blueprint-jira-better-excel/blueprint-belarus-planning-helper.groovy
@@ -1,2 +1,2 @@
 filename = "Belarus Release Planning - ${new Date().format("yyyy-MM-dd-HH-mm-ss-z", TimeZone.getTimeZone('EST'))}.xlsx".toString();
-issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND fixVersion in (Belarus) AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
+issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND fixVersion in (Belarus, "Canada (12.4)") AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
