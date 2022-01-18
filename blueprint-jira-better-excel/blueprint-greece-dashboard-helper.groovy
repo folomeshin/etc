d4db5c2918a757fade5844bd4b1b7fe40a7dca13
Alex Folomechine <alex.folomechine@blueprintsys.com>
2022-01-18 18:19:04 -0500

    For Greece

---
+++ blueprint-jira-better-excel/blueprint-greece-dashboard-helper.groovy
@@ -0,0 +1,2 @@
+filename = "Greece Dashboard - ${new Date().format("yyyy-MM-dd-HH-mm-ss-z", TimeZone.getTimeZone('EST'))}.xlsx".toString();
+issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND fixVersion in ("Greece (13.0)", "Task Capture 5.1") AND fixVersion not in  ("Houston (13.1)", "Future") AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
