eb3a2a9054577034f64306ed527378a61714f752
Alex Folomechine <alex.folomechine@blueprintsys.com>
2022-09-27 18:39:53 -0400

    For 5.6

---
+++ blueprint-jira-better-excel/blueprint-5-6-planning-helper.groovy
@@ -0,0 +1,2 @@
+filename = "5.6 Release Planning - ${new Date().format("yyyy-MM-dd-HH-mm-ss-z", TimeZone.getTimeZone('EST'))}.xlsx".toString();
+issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND fixVersion in ("AgileRM 13.4", "Blueprint 5.6") AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
