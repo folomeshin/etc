94d0593474458d6d93798f3cbd23ce720e229c04
Alex Folomechine <alex.folomechine@blueprintsys.com>
2024-07-23 19:52:21 -0400

    Final 7.4

--- blueprint-jira-better-excel/blueprint-7-4-planning-helper.groovy
+++ blueprint-jira-better-excel/blueprint-7-4-planning-helper.groovy
@@ -1,2 +1,2 @@
 filename = "7.4 Release Planning - ${new Date().format("yyyy-MM-dd-HH-mm-ss-z", TimeZone.getTimeZone('EST'))}.xlsx".toString();
-issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND fixVersion in ("Blueprint 7.4", "AgileRM 15.1") AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
+issues = bpHelper.searchIssues('project = Storyteller AND issuetype in (Epic, Story, Spike, "Tech Debt", DevOps, Bug) AND fixVersion in ("Blueprint 7.4", "Blueprint 7.5", "AgileRM 15.1") AND status not in ("Epic: Cancelled", "Story: Cancelled", "Tech Debt: Cancelled", "DevOps: Cancelled")');
